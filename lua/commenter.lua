4f45b038a69494f448546fc9aa9d8b1a5e0c91f6
Noah Wahl <noah.ares@yahoo.de>
2020-10-09 22:42:13 +0200

    Use vims in-built &commentstring

--- lua/commenter.lua
+++ lua/commenter.lua
@@ -1,31 +1,32 @@
 local api = vim.api
-local comment_strings = {}
-comment_strings['cpp'] = "//"
-comment_strings['sh'] = "#"
-comment_strings['lua'] = "--"
-comment_strings['vim'] = '"'
-comment_strings['tex'] = "%"
-local filetype, start_sel, end_sel, lines
+local start_sel, end_sel, lines, comment, comment_stripped
+
+local function get_comment_string()
+  comment = api.nvim_eval('&commentstring')
+  comment_stripped = string.format(comment, "")
+end
 
 local function add_comment_string(line)
-  return comment_strings[filetype] .. line
+  get_comment_string()
+  return string.format(comment, line)
 end
 
 local function remove_comment_string(line)
+  get_comment_string()
   local first_char = string.find(line, '%w')
-  local indices = string.find(line, comment_strings[filetype], 1, first_char)
-  return string.sub(line, indices + string.len(comment_strings[filetype]))
+  local indices = string.find(line, comment_stripped, 1, first_char)
+  return string.sub(line, indices + string.len(comment_stripped))
 end
 
-local function get_vars()
-  filetype = api.nvim_eval('&filetype')
+local function get_selection()
+  -- TODO handle reverse selection and single line selection
   start_sel = api.nvim_eval('getpos("\'<")[1]') - 1
   end_sel = api.nvim_eval('getpos("\'>")[1]')
   lines = api.nvim_buf_get_lines(0, start_sel, end_sel, true)
 end
 
 local function multi_commenter()
-  get_vars()
+  get_selection()
   for i,l in ipairs(lines) do
     lines[i] = add_comment_string(l)
   end
@@ -33,6 +34,7 @@ local function multi_commenter()
 end
 
 local function multi_uncommenter()
+  get_selection()
   for i,l in ipairs(lines) do
     lines[i] = remove_comment_string(l)
   end
@@ -40,14 +42,12 @@ local function multi_uncommenter()
 end
 
 local function single_commenter()
-  filetype = api.nvim_eval('&filetype')
   local line = api.nvim_get_current_line()
   line = add_comment_string(line)
   api.nvim_set_current_line(line)
 end
 
 local function single_uncommenter()
-  filetype = api.nvim_eval('&filetype')
   local line = api.nvim_get_current_line()
   line = remove_comment_string(line)
   api.nvim_set_current_line(line)
